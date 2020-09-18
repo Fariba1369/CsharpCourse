6b72f46e85fa233e6d9977dfa094477fd59123af
Hamed Kordlou <hamed@DESKTOP-BP6QIRS>
2020-09-18 16:45:20 +0200

    init if else, loops, class

---
+++ ConsoleApp2/MyList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class MyList
+    {
+        int[] array;
+        int[] array2 = { 5, 6, 7};
+
+        //public static void MyMetod(int input)
+        //{
+        //    // definition
+        //}
+
+        public MyList()
+        {
+            array = array2;
+        }
+
+        public MyList(int a, int b, int c)
+        {
+            int[] temp = { a, b, c};
+            array = temp;
+        }
+
+        public bool Contains(int input)        // 1, 2, 3, 4   Contains(3) => true
+        {
+            //for(int i=0; i< array.Length; i++)
+            //{
+            //    if (array[i] == input)
+            //        return true;
+            //}
+            foreach(int n in array)
+            {
+                if(n == input)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mylist = new MyList(1, 2, 3);
+            //MyList mylist = new MyList();
+
+            var res = mylist.Contains(3);
+
+            Console.WriteLine(res);
+
+            //var number = 10;
+
+            //string[] arry = { "a1", "b1", "c1" };
+
+            //var list = new List<int>() { 5, 2, 7 };
+
+            //list.Add("d1");
+            //var result = list.Contains("c2");
+            //list.ForEach(x => Console.WriteLine(x));
+            //Console.WriteLine();
+            //list.Sort();
+            //Console.WriteLine("count: " + list.Count);
+            //list.ForEach(x => Console.WriteLine(x));
+            //Console.WriteLine();
+
+            //foreach (var str in list)
+            //{
+            //    Console.WriteLine(str);
+            //}
+
+            //var i = 0;
+            //while(i < number)
+            //{
+            //    var j = 0;
+            //    while(j <= i)
+            //    {
+            //        Console.Write("*  ");
+            //        j++;
+            //    }
+            //    Console.WriteLine("");
+            //    i++;
+            //}
+
+
+
+            //for (int i = 0; i < number; i++)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write("*  ");
+            //        //Console.WriteLine("*");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            //if(number >= 10 && ctrl)
+            //{
+            //    Console.WriteLine("in if");
+            //}
+            //else if(t == 0)
+            //{
+            //    Console.WriteLine("in else if");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("in else");
+            //}
+        }
+    }
+}
